7fc1619a5baa305d45d8139d6f640c3d0ba02b3f
Inderpal Singh <gandaleve@gmail.com>
2022-02-23 16:57:09 -0800

    Created Entity.cs

---
+++ src/ECS/Entity.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Bulldog.ECS;
+
+public class Entity
+{
+    private Guid Id { get; }
+    public List<Component> ComponentPool { get; }
+
+    public void AddComponent(Component component)
+    {
+        ComponentPool.Add(component);
+        component.EntityId = this;
+    }
+    public T GetComponent<T>() where T : Component
+    {
+        return ComponentPool.Where(component => component.GetType().Equals(typeof(T))).Cast<T>().FirstOrDefault();
+    }
+
+    
+}
