434c3dba5e9f262a4d6de0adeb0fa3fc44c26e50
Inderpal Singh <gandaleve@gmail.com>
2022-04-12 14:09:24 -0700

    Added new SetUniform Overload for Matrix4X4

--- src/Renderer/Shader.cs
+++ src/Renderer/Shader.cs
@@ -1,4 +1,5 @@
 using Silk.NET.OpenGL;
+using System.Numerics;
 
 namespace Bulldog.Renderer
 {
@@ -41,6 +42,17 @@ namespace Bulldog.Renderer
                 throw new Exception($"{name} uniform not found on shader.");
             }
             _gl.Uniform1(location, value);
+        }
+        
+        public unsafe void SetUniform(string name, Matrix4x4 value)
+        {
+            //A new overload has been created for setting a uniform so we can use the transform in our shader.
+            int location = _gl.GetUniformLocation(_handle, name);
+            if (location == -1)
+            {
+                throw new Exception($"{name} uniform not found on shader.");
+            }
+            _gl.UniformMatrix4(location, 1, false, (float*) &value);
         }
 
         public void SetUniform(string name, float value)
