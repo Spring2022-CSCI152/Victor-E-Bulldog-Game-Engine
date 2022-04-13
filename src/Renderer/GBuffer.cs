7a95df727fa195f6fcceeb54f3d2ceb0ce2666e2
Inderpal Singh <gandaleve@gmail.com>
2022-04-13 14:16:19 -0700

    changes to gBuffer init

--- src/Renderer/GBuffer.cs
+++ src/Renderer/GBuffer.cs
@@ -1,66 +1,91 @@
 using Silk.NET.OpenGL;
+
 // ReSharper disable InconsistentNaming
 
 namespace Bulldog.Renderer;
 
 
-public unsafe class gBuffer
+public unsafe static class GBuffer
 {
-     private uint _handel;
-     private GL _gl;
-     public uint rboDepth;
-     public uint gAlebedo, gNormals, gPbr, gPostion;
-     private GLEnum[] attachments;
+     public static uint Handle { get; private set; }
 
-     public void init(uint height, uint width, GL gl)
+     private static GL _gl;
+     public static uint rboDepth { get; private set; }
+     public static uint gAlebedo { get; private set; }
+     public static uint gNormals { get; private set; }
+     public static uint gPbr { get; private set; }
+     public static uint gPostion { get; private set; }
+     private static GLEnum[] attachments;
+
+     public static void init(uint height, uint width, GL gl)
      {
           _gl = gl;
           // generate offscreen gBuffer
-          _gl.GenFramebuffers( 1, (uint*) _handel);
-          _gl.BindFramebuffer( FramebufferTarget.Framebuffer , _handel);
+          Handle = _gl.GenFramebuffer();
+          _gl.BindFramebuffer( FramebufferTarget.Framebuffer , Handle);
           
           // bind normal to a 2d texture in the gBuffer
-          _gl.GenTextures(1, (uint*) gNormals);
-          _gl.BindTexture(TextureTarget.Texture2D, gNormals );
-          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgb16f, GLEnum.Float , null);
+          gNormals = _gl.GenTexture();
+          _gl.BindTexture(TextureTarget.Texture2D, Handle );
+          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgba16f, GLEnum.Float , null);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
           _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, gNormals,0);
           //  bind postion to a 2d texture in the gBuffer
-          _gl.GenTextures(1, (uint*) gPostion);
-          _gl.BindTexture(TextureTarget.Texture2D, gPostion );
+          gPostion = _gl.GenTexture();
+          _gl.BindTexture(TextureTarget.Texture2D, Handle );
           _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgb16f, GLEnum.Float , null);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
           _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment1, TextureTarget.Texture2D, gPostion,0);
-          //  bind PBR maps to a 2d texture in the gBuffer
-          _gl.GenTextures(1, (uint*) gPbr);
-          _gl.BindTexture(TextureTarget.Texture2D, gPbr );
-          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.RG8, width, height, 0, GLEnum.UnsignedByte, GLEnum.Float , null);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
-          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment2, TextureTarget.Texture2D, gPbr,0);
           // bind albedo to a 2d texture in the gBuffer
-          _gl.GenTextures(1, (uint*) gAlebedo);
-          _gl.BindTexture(TextureTarget.Texture2D, gAlebedo );
+          gAlebedo = _gl.GenTexture();
+          _gl.BindTexture(TextureTarget.Texture2D, Handle );
           _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.UnsignedByte, GLEnum.Float , null);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
-          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment3, TextureTarget.Texture2D, gAlebedo,0);
+          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment2, TextureTarget.Texture2D, gAlebedo,0);
 
           // ReSharper disable once HeapView.ObjectAllocation.Evident
           attachments = new []
           {
-               (GLEnum) FramebufferAttachment.ColorAttachment0, (GLEnum) FramebufferAttachment.ColorAttachment1, (GLEnum) FramebufferAttachment.ColorAttachment2, (GLEnum) FramebufferAttachment.ColorAttachment3
+               GLEnum.ColorAttachment1, GLEnum.ColorAttachment1, GLEnum.ColorAttachment2
           };
 
-          _gl.DrawBuffers(4, attachments);
+          _gl.DrawBuffers(3, attachments);
 
-          _gl.GenRenderbuffers(1,(uint*) rboDepth);
+          rboDepth = _gl.GenRenderbuffer();
           _gl.BindRenderbuffer(RenderbufferTarget.Renderbuffer, rboDepth);
           _gl.RenderbufferStorage(RenderbufferTarget.Renderbuffer, GLEnum.DepthComponent, width, height);
           _gl.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, RenderbufferTarget.Renderbuffer, rboDepth);
 
+          var temp = _gl.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
+          if (_gl.CheckFramebufferStatus(FramebufferTarget.Framebuffer) != GLEnum.FramebufferComplete)
+          {
+               Console.WriteLine("framebuffer is not complete");
+          }
+
+          _gl.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+          
      }
 
 }
+// ReSharper disable once InvalidXmlDocComment
+/**
+public void Configure() {
+     GLEnum[] drawBuffers = new GLEnum[Textures.Count];
+     
+     for(int texId = 0; texId < Textures.Count; texId++) {
+     
+          Renderer.Gl.BindTexture(TextureTarget.Texture2D, Textures[texId].GetHandle());
+          Renderer.Gl.FramebufferTexture(GLEnum.Framebuffer, GLEnum.ColorAttachment0 + texId, Textures[texId].GetHandle(), 0)
+          ;
+          drawBuffers[texId] = GLEnum.ColorAttachment0 + texId;
+     }
+     Renderer.Gl.DrawBuffers((uint)Textures.Count, drawBuffers);
+            
+     if(Renderer.Gl.CheckFramebufferStatus(GLEnum.Framebuffer) != GLEnum.FramebufferComplete) {
+          System.Console.WriteLine("Framebuffer configured incorrectly");
+          System.Environment.Exit(-1);
+     }
+}*/
