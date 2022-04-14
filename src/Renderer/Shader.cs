ba8739073c349c832fffc873463e7779b8884e47
Inderpal Singh <gandaleve@gmail.com>
2022-04-14 15:28:27 -0700

    Added function overload to Set Vec3 uniform

--- src/Renderer/Shader.cs
+++ src/Renderer/Shader.cs
@@ -65,6 +65,16 @@ namespace Bulldog.Renderer
             _gl.Uniform1(location, value);
         }
 
+        public void SetUniform(string name, Vector3 value)
+        {
+            int location = _gl.GetUniformLocation(_handle, name);
+            if (location == -1)
+            {
+                throw new Exception($"{name} uniform not found on shader.");
+            }
+            _gl.Uniform3(location, value.X, value.Y, value.Z);
+        }
+        
         public void Dispose()
         {
             _gl.DeleteProgram(_handle);
