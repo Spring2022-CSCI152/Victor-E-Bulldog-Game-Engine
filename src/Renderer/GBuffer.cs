858f7689fcd6b98b76b4be2ef4ec0b04863c37b1
Inderpal Singh <gandaleve@gmail.com>
2022-04-13 21:14:35 -0700

    Fixed bug with incomplete framebuffer attachment during init

--- src/Renderer/GBuffer.cs
+++ src/Renderer/GBuffer.cs
@@ -22,45 +22,45 @@ public unsafe static class GBuffer
           _gl = gl;
           // generate offscreen gBuffer
           Handle = _gl.GenFramebuffer();
-          _gl.BindFramebuffer( FramebufferTarget.Framebuffer , Handle);
+          _gl.BindFramebuffer( GLEnum.Framebuffer , Handle);
           
-          // bind normal to a 2d texture in the gBuffer
-          gNormals = _gl.GenTexture();
-          _gl.BindTexture(TextureTarget.Texture2D, Handle );
-          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgba16f, GLEnum.Float , null);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
-          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, gNormals,0);
-          //  bind postion to a 2d texture in the gBuffer
           gPostion = _gl.GenTexture();
-          _gl.BindTexture(TextureTarget.Texture2D, Handle );
-          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgb16f, GLEnum.Float , null);
+          _gl.BindTexture(TextureTarget.Texture2D, gPostion );
+          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba16f, width, height, 0, GLEnum.Rgba, GLEnum.Float , null);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
-          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment1, TextureTarget.Texture2D, gPostion,0);
-          // bind albedo to a 2d texture in the gBuffer
+          _gl.FramebufferTexture2D(GLEnum.Framebuffer, GLEnum.ColorAttachment0, TextureTarget.Texture2D, gPostion,0);
+          
+          gNormals = _gl.GenTexture();
+          _gl.BindTexture(TextureTarget.Texture2D, gNormals );
+          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba16f, width, height, 0, GLEnum.Rgba, GLEnum.Float , null);
+          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
+          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
+          _gl.FramebufferTexture2D(GLEnum.Framebuffer, GLEnum.ColorAttachment1, TextureTarget.Texture2D, gNormals,0);
+          
           gAlebedo = _gl.GenTexture();
-          _gl.BindTexture(TextureTarget.Texture2D, Handle );
-          _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.UnsignedByte, GLEnum.Float , null);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
-          _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
-          _gl.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment2, TextureTarget.Texture2D, gAlebedo,0);
-
+           _gl.BindTexture(TextureTarget.Texture2D, gAlebedo );
+           _gl.TexImage2D(TextureTarget.Texture2D, 0, (int) InternalFormat.Rgba, width, height, 0, GLEnum.Rgba, GLEnum.Float , null);
+           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMinFilter,(uint) GLEnum.Nearest);
+           _gl.TexParameterI(TextureTarget.Texture2D,GLEnum.TextureMagFilter,(uint) GLEnum.Nearest);
+           _gl.FramebufferTexture2D(GLEnum.Framebuffer, GLEnum.ColorAttachment2, TextureTarget.Texture2D, gAlebedo,0);
           // ReSharper disable once HeapView.ObjectAllocation.Evident
+          //gl.TexImage2D(/*target*/, 0, /* internal format */, image.Width, image.Height, 0, /* format */, /* type */, null);
+          
           attachments = new []
           {
-               GLEnum.ColorAttachment1, GLEnum.ColorAttachment1, GLEnum.ColorAttachment2
+               GLEnum.ColorAttachment0, GLEnum.ColorAttachment1, GLEnum.ColorAttachment2
           };
 
-          _gl.DrawBuffers(3, attachments);
+          _gl.DrawBuffers(attachments);
 
           rboDepth = _gl.GenRenderbuffer();
           _gl.BindRenderbuffer(RenderbufferTarget.Renderbuffer, rboDepth);
           _gl.RenderbufferStorage(RenderbufferTarget.Renderbuffer, GLEnum.DepthComponent, width, height);
           _gl.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, RenderbufferTarget.Renderbuffer, rboDepth);
 
-          var temp = _gl.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
-          if (_gl.CheckFramebufferStatus(FramebufferTarget.Framebuffer) != GLEnum.FramebufferComplete)
+          var temp = _gl.CheckFramebufferStatus(GLEnum.Framebuffer);
+          if (_gl.CheckFramebufferStatus(GLEnum.Framebuffer) != GLEnum.FramebufferComplete)
           {
                Console.WriteLine("framebuffer is not complete");
           }
@@ -70,22 +70,3 @@ public unsafe static class GBuffer
      }
 
 }
-// ReSharper disable once InvalidXmlDocComment
-/**
-public void Configure() {
-     GLEnum[] drawBuffers = new GLEnum[Textures.Count];
-     
-     for(int texId = 0; texId < Textures.Count; texId++) {
-     
-          Renderer.Gl.BindTexture(TextureTarget.Texture2D, Textures[texId].GetHandle());
-          Renderer.Gl.FramebufferTexture(GLEnum.Framebuffer, GLEnum.ColorAttachment0 + texId, Textures[texId].GetHandle(), 0)
-          ;
-          drawBuffers[texId] = GLEnum.ColorAttachment0 + texId;
-     }
-     Renderer.Gl.DrawBuffers((uint)Textures.Count, drawBuffers);
-            
-     if(Renderer.Gl.CheckFramebufferStatus(GLEnum.Framebuffer) != GLEnum.FramebufferComplete) {
-          System.Console.WriteLine("Framebuffer configured incorrectly");
-          System.Environment.Exit(-1);
-     }
-}*/
