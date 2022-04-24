e9f097ff77447f3b07073eee919c59aab189243d
Inderpal Singh <gandaleve@gmail.com>
2022-04-24 16:08:08 -0700

    Added new enum for LightingTypes

--- src/Core/Enums.cs
+++ src/Core/Enums.cs
@@ -2,10 +2,17 @@ namespace Bulldog.Core;
 
 public enum RenderTarget
 {
-    
 }
 
 public enum TextureTarget
 {
     
+}
+
+public enum LightingType
+{
+    Point = 1,
+    Directional = 0,
+    Spot = 2,
+    SoftSpot = 3
 }
